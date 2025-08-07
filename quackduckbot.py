ef935c6fd781f2c654ae0d2b84e6c5f8e31271cf
Vova <volodymyr.kholodkov@gmail.com>
2025-08-07 12:26:36 +0200

    welcom to QuackDuckBot

--- quackduckbot.py
+++ quackduckbot.py
@@ -9,25 +9,30 @@ from telegram.ext import (
 )
 
 async def msg_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message = update.message
-    print(f"Received message: {message.text}")
+    message = update.message.text
+    print(f"Received message: {message}")
 
 async def cmd_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     command = update.message.text
     print(f"Received command: {command}")
+    if command == '/start':
+        user_first_name = update.message.from_user.first_name if update.message.from_user else "there"
+        reply_text = f"Hello {user_first_name}! Welcome to Quack Duck Bot."
+        await update.message.reply_text(reply_text)
+
 
 def main():
     load_dotenv()
-    token = os.getenv('QUACK_DUCK_BOT_TOKEN')
-    if token is None:
+    tbot_token = os.getenv('QUACK_DUCK_BOT_TOKEN')
+    if tbot_token is None:
         raise ValueError("QUACK_DUCK_BOT_TOKEN environment variable is not set")
 
-    application = Application.builder().token(token).build()
+    tbot_application = Application.builder().token(tbot_token).build()
 
-    application.add_handler(MessageHandler(~filters.COMMAND, msg_handler))
-    application.add_handler(MessageHandler(filters.COMMAND, cmd_handler))
+    tbot_application.add_handler(MessageHandler(~filters.COMMAND, msg_handler))
+    tbot_application.add_handler(MessageHandler(filters.COMMAND, cmd_handler))
 
-    application.run_polling()
+    tbot_application.run_polling()
 
 if __name__ == '__main__':
     main()
